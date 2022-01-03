2dd0afaa2f102003a6d722712e14714189234a00
hailie1230@naver.com <66510673+hailie1230@users.noreply.github.com>
2022-01-04 00:05:08 +0900

    포트폴리오 index03
    스크립트 추가

---
+++ port/assets/js/script.js
@@ -0,0 +1,7 @@
+// const toggleBtn = document.querySelector('.rightBtn');
+// const gnbWrap = document.querySelector('.gnbWrap');
+
+// toggleBtn.addEventListener('click', () => {
+//     gnbWrap.classList.toggle('active');
+    
+// });
